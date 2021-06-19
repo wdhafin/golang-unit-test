c93a505b13ab2910b6b0a0f58b63672263294ad4
wdhafin <work.dhafin@gmail.com>
2021-06-19 11:29:16 +0700

    add error test

--- pkg/helper/hello_world_test.go
+++ pkg/helper/hello_world_test.go
@@ -1,11 +1,24 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Dhafin")
 	if result != "Hello Dhafin" {
 		// error
-		panic("result is not 'Hello Dhafin'")
+		t.Error("Result must be 'Hello Dhafin'")
 	}
+	fmt.Println("TestHelloWorld done")
+}
+
+func TestHelloWorldRizqullah(t *testing.T) {
+	result := HelloWorld("Rizqullah")
+	if result != "Hello Rizqullah" {
+		// error
+		t.Fatal("Result must be 'Hello Rizqullah'")
+	}
+	fmt.Println("TestHelloWorldRizqullah done")
 }
